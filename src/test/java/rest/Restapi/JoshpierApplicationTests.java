345b3ad045d08d8f66b4853e6fc95c44b4a7f3d2
NYESIGA <nyesigah65@gmail.com>
2023-02-25 06:52:04 +0800

    added sout statement

---
+++ src/test/java/rest/Restapi/JoshpierApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.Restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoshpierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
