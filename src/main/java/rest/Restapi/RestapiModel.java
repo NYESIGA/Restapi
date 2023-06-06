6fdcc230f06bdafc9e3665405ea4467305d21e13
NYESIGA <nyesiga65@gmail.com>
2023-06-06 23:48:35 +0300

    added

---
+++ src/main/java/rest/Restapi/RestapiModel.java
@@ -0,0 +1,32 @@
+package rest.Restapi;
+
+public class RestapiModel {
+private long id;
+ private String name;
+private String email;
+private  String passward;
+public long getId() {
+    return id;
+}
+public void setId(long id) {
+    this.id = id;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+public String getEmail() {
+    return email;
+}
+public void setEmail(String email) {
+    this.email = email;
+}
+public String getPassward() {
+    return passward;
+}
+public void setPassward(String passward) {
+    this.passward = passward;
+}
+}
